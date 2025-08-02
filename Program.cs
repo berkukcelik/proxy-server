128bdaf493ebe20eab154c8057c26de1a8ca5769
berkukcelik <berkukcelik124@hotmail.com>
2025-08-02 14:33:01 +0300

    cli command parsing

--- Program.cs
+++ Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.CommandLine;
+using System.CommandLine.Parsing;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var rootCommand = AddingCommands.ConfigureRootCommand(args);
+        
+    }
+}
+
+ 
+        
